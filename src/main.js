dde6e81f3f4c13d7783a73f58f6b41190f06d239
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-09-24 14:45:04 +0300

    header

--- src/main.js
+++ src/main.js
@@ -3,3 +3,15 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+
+const bars = document.getElementById('bars')
+bars.addEventListener('click',displaySidebar)
+function displaySidebar() {
+    const sidebar = document.getElementById('sidebar')
+    if(sidebar.style.width == 0 || sidebar.style.width == '0px') {
+        sidebar.style.width = '250px';
+    }else {
+        sidebar.style.width = '0'
+        console.log(0)
+    }
+}
